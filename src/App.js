57dd333b85663479fcfddf0333a1bd38a673c91d
AntonioBukin <bav1905@gmail.com>
2023-06-22 15:03:27 +0300

    add some informations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Navbar from "./modules/Navbar/Navbar";
+import ToggleButton from "./shared/components/ToggleButton/ToggleButton";
 
 import navbarMenuItems from "./data/navbarMenuItems.json"
 
@@ -6,7 +7,12 @@ import "./shared/styles/styles.scss";
 
 function App() {
   return (
-   <Navbar menuItems={navbarMenuItems} />
+    <>
+    <Navbar menuItems={navbarMenuItems} />
+    <div className="container">
+    <ToggleButton text="Click me" />
+    </div>
+    </>
   );
 }
 
