db34237cc85fbdf8ae29ba3b21ad07525764bac8
AntonioBukin <bav1905@gmail.com>
2023-06-26 11:46:49 +0300

    add inf2

--- src/modules/Vote/VoteBlock/VoteBlock.jsx
+++ src/modules/Vote/VoteBlock/VoteBlock.jsx
@@ -1,1 +1,11 @@
 import styles from "./vote-block.module.scss"
+
+const VoteBlock = ({title}) => {
+    return (
+        <div className={styles.block}>
+            <h4 className={styles.blockTitle}>{title}</h4>
+        </div>
+    )
+}
+
+export default VoteBlock;
--- src/modules/Vote/VoteResult/VoteResult.jsx
+++ src/modules/Vote/VoteResult/VoteResult.jsx
@@ -2,12 +2,10 @@ import styles from "../vote.module.scss";
 
 const VoteResult = ({total, democratesPercent, republicansPercent}) => {
     return (
-        <div className={styles.block}>
-            <h4 className={styles.blockTitle}>Results</h4>
-            <p>Total votes: {total}</p>
-            <p>Republicans: {republicansPercent}%</p>
-            <p>Democrats: {democratesPercent}%</p>
-        </div>
+        <>
+        <p>Total votes: {total}</p>
+        <p>Republicans: {republicansPercent}%</p>
+        <p>Democrats: {democratesPercent}%</p></>
     )
 }
 
